2464a4feaff250626d46070e84f622631faf78cf
keepConcentration <phm543@gmail.com>
2022-07-19 08:31:03 +0900

    컴포넌트 스캔 강의 소스코드

--- 2. 스프링 핵심 원리 이해1/core/src/main/java/com/waw/core/discount/RateDiscountPolicy.java
+++ 2. 스프링 핵심 원리 이해1/core/src/main/java/com/waw/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.waw.core.discount;
 
 import com.waw.core.member.Grade;
 import com.waw.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
