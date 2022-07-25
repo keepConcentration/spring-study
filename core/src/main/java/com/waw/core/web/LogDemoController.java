d0dae2154cfec198ff792dd74c037021ae8dd50c
keepConcentration <phm543@gmail.com>
2022-07-26 08:55:36 +0900

    스코프와 프록시 소스

--- core/src/main/java/com/waw/core/web/LogDemoController.java
+++ core/src/main/java/com/waw/core/web/LogDemoController.java
@@ -17,19 +17,20 @@ public class LogDemoController {
 
     // scope: request.
     // httpRequest가 없어서 MyLogger가 빈으로 생성이 안됨, 따라서 스프링 컨테이너에 컨트롤러를 싱글톤 빈으로 등록할 때 MyLogger 주입이 불가함.
-    // private final MyLogger myLogger;
+    private final MyLogger myLogger;
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         // MyLogger에 requestURL 세팅하는 부분은 스프링 인터셉터나 서블릿 필터에서 공통처리하는 것이 좋다.
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
 
         return "OK";
