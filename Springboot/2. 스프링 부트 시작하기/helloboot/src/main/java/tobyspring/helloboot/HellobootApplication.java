42f18c8e69971c7c9df1278f2d52855cb1d48b96
keepConcentration <phm543@gmail.com>
2023-02-11 12:37:05 +0900

    서블릿 컨테이너 띄우기

--- Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,24 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// 서블릿컨테이너 실행 (임베디드 톰캣)
+		// new Tomcat().start();
+
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+
+		// 톰캣 외의 제티같은 다른 서블릿 컨테이너를 지원하기 위해 추상화.
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
