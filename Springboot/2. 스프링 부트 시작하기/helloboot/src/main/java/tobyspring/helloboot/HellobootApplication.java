42870c040ab073390319fd5ca2474fc566cf4d32
keepConcentration <phm543@gmail.com>
2023-02-18 11:01:19 +0900

    서블릿 요청 처리

--- Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFa
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
@@ -17,34 +20,30 @@ import java.io.IOException;
 
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		// 서블릿컨테이너 실행 (임베디드 톰캣)
-		// new Tomcat().start();
+    public static void main(String[] args) {
+        // 서블릿컨테이너 실행 (임베디드 톰캣)
+        // new Tomcat().start();
 
-		// 톰캣 외의 제티, 토우같은 다른 서블릿 컨테이너를 지원하기 위해 추상화된 인터페이스
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		// ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
-		// ServletWebServerFactory serverFactory = new UndertowServletWebServerFactory();
+        // 톰캣 외의 제티, 토우같은 다른 서블릿 컨테이너를 지원하기 위해 추상화된 인터페이스
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+        // ServletWebServerFactory serverFactory = new UndertowServletWebServerFactory();
 
+        // ServletContextInitializer 에 위 서블릿 등록, 위 서블릿은 /hello URL 요청을 처리한다.
+        WebServer webServer = serverFactory.getWebServer(servletContext ->
+                servletContext.addServlet("hello", new HttpServlet() {
+					// 요청을 수행할 서블릿
+                    @Override
+                    protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+                        String name = req.getParameter("name");
 
-		// 요청을 수행할 서블릿
-		Servlet httpServlet = new HttpServlet() {
-			@Override
-			protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-				res.setStatus(200);
-				res.setHeader("Content-Type", "text/plain");
-				res.getWriter().print("Hello Servlet");
-			}
-		};
-
-		// ServletContextInitializer 에 위 서블릿 등록, 위 서블릿은 /hello URL 요청을 처리한다.
-		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				servletContext.addServlet("hello", httpServlet).addMapping("/hello");
-			}
-		});
-		webServer.start();
-	}
+                        res.setStatus(HttpStatus.OK.value());
+                        res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        res.getWriter().println("Hello " + name);
+                    }
+                }).addMapping("/hello"));
+        // 요청: http -v ":8080/hello?name=Spring"
+        webServer.start();
+    }
 
 }
