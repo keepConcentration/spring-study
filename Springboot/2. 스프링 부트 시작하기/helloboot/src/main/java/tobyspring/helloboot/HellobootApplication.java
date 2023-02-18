ba676af87aec2f082cac9a37ab3c51597f47aa6b
keepConcentration <phm543@gmail.com>
2023-02-18 12:36:35 +0900

    스프링 컨테이너로 통합

--- Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -28,7 +28,29 @@ public class HellobootApplication {
     public static void main(String[] args) {
 
         // 스프링 컨테이너
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            // 스프링 컨테이너 초기화 후 서블릿 컨테이너 초기화 작업을 스프링 컨테이너 초기화 시 서블릿 컨테이너도 함께 초기화하는 작업을 위해
+            // onRefresh()를 재정의함.
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext ->
+                        servletContext.addServlet("frontcontroller",
+                                // DispatcherServlet은 "Web"ApplicationContext를 파라미터로 넣어줘야한다.
+                                // 각 URL, Method에 대해 처리할 서블릿에 대한 힌트가 없어 어떤 요청이라도 404.
+                                // 해결 방법은 해당 URL, Method 요청을 처리할 컨트롤러에 매핑정보를 입력.
+                                // @RequestMapping, @GetMapping과 같은 URL 매핑 정보를 사용해서 웹 요청을 위임함.
+                                // Controller가 String을 리턴하면 View로 인식함.
+                                // String을 웹 응답 Body에 넣기 위해선 @ResponseBody를 사용.
+                                // @RestController를 사용하면 클래스 아래 메소드에 모두 @ResponseBody를 붙임
+                                new DispatcherServlet(this)
+                        ).addMapping("/*"));
+                webServer.start();
+
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();  // 구성정보를 이용해 컨테이너 초기화
@@ -36,11 +58,11 @@ public class HellobootApplication {
         // 서블릿컨테이너 실행 (임베디드 톰캣)
         // new Tomcat().start();
 
+        // 서블릿 컨테이너 초기화
         // ServletWebServerFactory: 톰캣 외의 제티, 토우같은 다른 서블릿 컨테이너를 지원하기 위해 추상화된 인터페이스
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
         // ServletWebServerFactory serverFactory = new UndertowServletWebServerFactory();
-
+        /*ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext ->
                 servletContext.addServlet("frontcontroller",
                         // DispatcherServlet은 "Web"ApplicationContext를 파라미터로 넣어줘야한다.
@@ -52,7 +74,7 @@ public class HellobootApplication {
                         // @RestController를 사용하면 클래스 아래 메소드에 모두 @ResponseBody를 붙임
                         new DispatcherServlet(applicationContext)
                 ).addMapping("/*"));
-        webServer.start();
+        webServer.start();*/
 
 
         /*
