57dc61a74b3cba6a4b69cf57633de7ed0eb95bf8
keepConcentration <phm543@gmail.com>
2023-03-04 12:57:02 +0900

    동적인 자동 구성 파일 분리

--- Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ Springboot/2. 스프링 부트 시작하기/helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,37 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
+        List<String> autoConfigs = new ArrayList<>();
+
+        // META-INF/spring/패키지명 포함한 파일명.imports 파일을 읽음
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+        return autoConfigs.stream().toArray(String[]::new);
+
+
+        // Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        // return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+
+
+        /*return new String[] {
                 "tobyspring.config.autoconfig.DispatcherServletConfig",
                 "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+        };*/
     }
+
 }
